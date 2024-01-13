5144b6cb8c4143444ab0bbdbc25f6978c376f4cd
baharkose <baharkse17@gmail.com>
2024-01-13 09:42:50 +0300

    fix: menu list item

--- src/components/MenuListITems.jsx
+++ src/components/MenuListITems.jsx
@@ -1,17 +1,15 @@
-import InboxIcon from "@mui/icons-material/MoveToInbox";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
 import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
 import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 import InventoryIcon from "@mui/icons-material/Inventory";
 import StoreIcon from "@mui/icons-material/Store";
 import StarsIcon from "@mui/icons-material/Stars";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const icons = [
   {
@@ -20,7 +18,7 @@ const icons = [
     url: "/stock/",
   },
   {
-    title: "Purchase",
+    title: "Purchases",
     icon: <ShoppingCartIcon />,
     url: "/stock/purchases/",
   },
@@ -51,9 +49,25 @@ const MenuListItems = () => {
   return (
     <List>
       {icons.map((item, index) => (
-        <ListItem key={index} disablePadding onClick={() => navigate(item.url)}>
+        <ListItem
+          sx={{
+            color: "white",
+            "&:hover": { color: "red" },
+            "&:hover .MuiSvgIcon-root": { color: "red" },
+            // listiteme tıklandığında iconun rengini kırmızı yap.
+            "& .MuiSvgIcon-root": { color: "white" },
+
+            // - nested seçim nested ile iconu beyaz yaptık. svglerin kırmızı olması için yine nested yapı ile yapıldı. Class seviyesine inip stillendirme işlemi yapma. nested yapılarda üstteki bir şeyin hoverı ile altlardaki bişeyi kontrol edebiliyoruz.Ancak burda kural şu illaki nesting olması lazım.
+          }}
+          key={index}
+          disablePadding
+          onClick={() => navigate(item.url)}
+        >
           <ListItemButton>
-            <ListItemIcon>{item.icon}</ListItemIcon>
+            {/* ikonlar yalnızca ilgili bulundukları alanda stillendi. mutlaka svgnin kendi bileşeninden stillendirme yapılmalıdır. */}
+            <ListItemIcon>
+              {item.icon}
+            </ListItemIcon>
             <ListItemText primary={item.title} />
           </ListItemButton>
         </ListItem>
